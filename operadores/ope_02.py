1f532448be2814f1a11761a0ed266190e1cbb293
Godoy <viniciusgodoy.martins@gmail.com>
2024-11-21 10:03:01 -0300

    Commit inicial - Operadores Matemáticos

---
+++ operadores/ope_02.py
@@ -0,0 +1,6 @@
+a = input("informe um valor para um numero:")
+a = int(a)
+b = input( "informe um valor para a potencia:")
+b = int(b)
+pot = a**b
+print (f"O valor {a} elevado a {b} é {pot}.")
---
+++ operadores/ope_03.py
@@ -0,0 +1,6 @@
+lado = input("digite o valor do lado do quadrado:")
+lado= int(lado)
+
+área= lado**2
+
+print(f'A área do quadrado é {área}')
